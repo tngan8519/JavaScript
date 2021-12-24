f5c100241dfd82f081e854acdd17b09c487e8374
tngan8519 <nganthithaotran@gmail.com>
2021-12-23 20:12:53 -0600

    second commit

---
+++ app.js
@@ -0,0 +1,87 @@
+class Media{
+    constructor(title){
+      this._title = title;
+      this._isCheckedOut = false;
+      this._ratings = [];
+    }
+    get title(){
+      return this._title;
+    }
+    get isCheckedOut(){
+      return this._isCheckedOut;
+    }
+    get ratings(){
+      return this._rating;
+    }
+    set isCheckedOut(value){
+      this._isCheckedOut = value;
+    }
+    toggleCheckOutStatus(){
+      this._isCheckedOut = !this._isCheckedOut;
+    }
+    getAverageRating(){
+      return this._ratings.reduce((a,b) => a+b)/this._ratings.length;
+    }
+    addRating(newRating){
+      this._ratings.push(newRating)
+    }
+  }
+  class Book extends Media{
+    constructor(author, title, pages){
+      super(title);
+      this._author = author;
+      this._pages = pages;
+    }
+    get author(){
+      return this._author;
+    }
+    get pages(){
+      return this._pages;
+    }
+  }
+  class Movie extends Media{
+    constructor(director, title, runTime){
+      super(title);
+      this._director = director;
+      this._runTime = runTime;
+    }
+     get director(){
+      return this._director;
+    }
+    get runTime(){
+      return this._runTime;
+    }
+  }
+  const historyOfEverything = new Book('Bill Bryson','A Short History of Nearly Everything',544)
+  historyOfEverything.toggleCheckOutStatus()
+  console.log(historyOfEverything.isCheckedOut)
+  historyOfEverything.addRating(4)
+  historyOfEverything.addRating(5)
+  historyOfEverything.addRating(5)
+  console.log(historyOfEverything.getAverageRating())
+  const speed = new Movie('Jan de Bont','Speed',116)
+  speed.toggleCheckOutStatus()
+  console.log(speed.isCheckedOut)
+  speed.addRating(1)
+  speed.addRating(1)
+  speed.addRating(5)
+  console.log(speed.getAverageRating())
+  
+  class CD extends Media{
+    constructor(title,songs){
+      super(title);
+      this._songs = [];
+    }
+    get songs(){
+      return this._songs;
+    }
+     addRating(newRating){
+       if(newRating >= 1 && newRating <=5){
+        this._ratings.push(newRating)
+       }
+    }
+    shuffle(){
+      const ramdomIndex = Math.floor(Math.random()*this._songs.length);
+      return songs[ramdomIndex];
+    }
+  }
